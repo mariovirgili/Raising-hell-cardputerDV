2d2c0dbe56ae1f59ee3319a8883d4baecc090b49
Aaron Ayers <acp.ayers@gmail.com>
2026-02-25 16:00:27 -0600

    Remove legacy menu_actions suppression; dedupe console includes

--- src/ui_state_console.cpp
+++ src/ui_state_console.cpp
@@ -4,31 +4,31 @@
 #include "console.h"
 #include "input.h"
 #include "settings_flow_state.h"
+#include "ui_input_common.h"
 #include "ui_input_utils.h"
-#include "ui_input_common.h"
 #include "ui_runtime.h"
-#include "ui_input_common.h"
-#include "ui_suppress.h"
 #include "ui_suppress.h"
 
 // Console return target
-static UIState      g_consoleReturnState      = UIState::PET_SCREEN;
-static Tab          g_consoleReturnTab        = Tab::TAB_PET;
-static bool         g_consoleReturnToSettings = false;
-static SettingsPage g_consoleReturnPage       = SettingsPage::TOP;
+static UIState g_consoleReturnState = UIState::PET_SCREEN;
+static Tab g_consoleReturnTab = Tab::TAB_PET;
+static bool g_consoleReturnToSettings = false;
+static SettingsPage g_consoleReturnPage = SettingsPage::TOP;
 
-void openConsoleWithReturn(UIState returnState, Tab returnTab, bool retToSettings, SettingsPage retSettingsPage) {
-  g_consoleReturnState      = returnState;
-  g_consoleReturnTab        = returnTab;
+void openConsoleWithReturn(UIState returnState, Tab returnTab, bool retToSettings, SettingsPage retSettingsPage)
+{
+  g_consoleReturnState = returnState;
+  g_consoleReturnTab = returnTab;
   g_consoleReturnToSettings = retToSettings;
-  g_consoleReturnPage       = retSettingsPage;
+  g_consoleReturnPage = retSettingsPage;
 
   consoleOpen();
   g_app.uiState = UIState::CONSOLE;
   requestUIRedraw();
 }
 
-static inline void swallowTypingAndEdges(InputState& in) {
+static inline void swallowTypingAndEdges(InputState &in)
+{
   // If we just returned from text-entry states, the key buffer can still have junk.
   uiDrainKb(in);
 
@@ -41,27 +41,32 @@ static inline void swallowTypingAndEdges(InputState& in) {
   clearInputLatch();
 }
 
-void uiConsoleHandle(InputState& input) {
-  if (input.menuOnce || input.escOnce) {
+void uiConsoleHandle(InputState &input)
+{
+  if (input.menuOnce || input.escOnce)
+  {
     consoleClose();
 
-    if (g_consoleReturnToSettings) {
+    if (g_consoleReturnToSettings)
+    {
       g_settingsFlow.settingsReturnValid = true;
       g_settingsFlow.settingsReturnState = g_consoleReturnState;
-      g_settingsFlow.settingsReturnTab   = g_consoleReturnTab;
-      g_settingsFlow.settingsPage        = g_consoleReturnPage;
+      g_settingsFlow.settingsReturnTab = g_consoleReturnTab;
+      g_settingsFlow.settingsPage = g_consoleReturnPage;
 
       g_app.uiState = UIState::SETTINGS;
       requestUIRedraw();
       swallowTypingAndEdges(input);
-      uiSuppressMenuForMs(250);      return;
+      uiSuppressMenuForMs(250);
+      return;
     }
 
-    g_app.uiState    = g_consoleReturnState;
+    g_app.uiState = g_consoleReturnState;
     g_app.currentTab = g_consoleReturnTab;
     requestUIRedraw();
     swallowTypingAndEdges(input);
-    uiSuppressMenuForMs(250);    return;
+    uiSuppressMenuForMs(250);
+    return;
   }
 
   // Let the console module handle keystrokes, cursor, etc.
